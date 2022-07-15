7835902ac563104d723fee6d7681d8ac37768bc8
Lucas Bispo <78982351+Master-Lukaa@users.noreply.github.com>
2022-07-14 22:32:48 -0300

    50% Concluido :)

---
+++ site/js/changeAnime.js
@@ -0,0 +1,24 @@
+
+function changeAnime() {
+   
+    var escolhaAnime = anime.value
+
+    if(escolhaAnime == "op"){
+        window.location.href = "./onePiece/homeOP.html";
+
+    }else if(escolhaAnime == "gl"){
+
+        window.location.href = "./gurrenLagann/homeGL.html";
+
+
+    }
+
+
+
+   
+   
+ 
+ 
+   }
+
+  
